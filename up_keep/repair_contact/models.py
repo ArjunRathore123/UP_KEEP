000827c93b2fc3ecdcbb664ef84d0a9c0a3c5430
ArjunRathore123 <123184191+ArjunRathore123@users.noreply.github.com>
2023-01-30 19:52:13 +0530

    adding initial commited

--- up_keep/repair_contact/models.py
+++ up_keep/repair_contact/models.py
@@ -6,8 +6,13 @@ class RepairContact(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(verbose_name='email', max_length=255)
     contact_no = models.CharField(max_length=10)
-    type_of_repairs = models.CharField(max_length=100)
-
+    MY_CHOICES = (
+        ('select option', 'select option'),
+        ('plumber', 'plumber'),
+        ('electrician', 'electrician'),
+        ('door repair', 'door repair')
+    )
+    type_of_repairs = models.CharField(max_length=100, choices=MY_CHOICES)
 
 
 
